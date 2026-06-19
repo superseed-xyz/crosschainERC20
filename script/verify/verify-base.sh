63b86d9ce5ef4159af7551a717bbe55c1949b3e4
0xNick <3832465+0xNikolas@users.noreply.github.com>
2026-06-19 14:47:11 +0300

    chore: ignore .idea and update base verification script

--- script/verify/verify-base.sh
+++ script/verify/verify-base.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Setup
-forge fmt && forge clean && forge build && source .env
+TOKEN_ADDRESS=0x17906b1Cd88aA8EfaEfC5e82891B52a22219BD45
+ETHERSCAN_VERIFIER_URL=https://api.basescan.org/api/
+CONSTRUCTOR_ARGS=$(cast ae "constructor(string,string,uint8,address)" "Superseed" "SUPR" 18 0xeF0834C8D531DA628E5f3985AdbaC44aaAfF4148)
 
 # Verify
 
 ## CrosschainERC20
-### Basescan
+### Etherscan
 forge verify-contract \
-0x17906b1Cd88aA8EfaEfC5e82891B52a22219BD45 \
-src/contracts/CrosschainERC20.sol:CrosschainERC20 \
---rpc-url base \
---etherscan-api-key $API_KEY_BASESCAN \
---verifier-url https://api.basescan.org/api/
-
-### Blockscout
-forge verify-contract \
-0x17906b1Cd88aA8EfaEfC5e82891B52a22219BD45 \
-src/contracts/CrosschainERC20.sol:CrosschainERC20 \
---rpc-url base \
---verifier blockscout \
---verifier-url https://base.blockscout.com/api/
+  $TOKEN_ADDRESS \
+  src/contracts/CrosschainERC20.sol:CrosschainERC20 \
+  --chain-id 8453 \
+  --watch \
+  --compiler-version "v0.8.25" \
+  --constructor-args "$CONSTRUCTOR_ARGS" \
+  --verifier-url $ETHERSCAN_VERIFIER_URL \
+  --etherscan-api-key "$API_KEY_BASESCAN"
